a19cd6dcd804d741e6186055ef48c20e7b78533f
Aarya <amathrej@asu.edu>
2022-05-28 17:32:51 -0700

    Add winner? and stalemate? method

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 class Board
   def initialize
     @board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    @winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     @game_over = false
     @available_moves = 9
 
@@ -32,30 +32,61 @@ class Board
     end
   end
 
-  public # change to protected later
+  def place_move_helper(move, player)
+    unless stalemate?
+      move_index = board_array.index(move)
 
-  def place_move(move, player)
-    move_index = board_array.index(move)
+      if move_index.nil?
+        puts 'Invalid choice, try again!'
 
-    if move_index.nil?
-      puts 'Invalid choice, try again!'
+      else
+        board_array[move_index] = player.player_symbol
+        self.available_moves -= 1
+        winner?(player)
+      end
+
+      create_board
 
     else
-      board_array[move_index] = player.player_symbol
-      self.available_moves -= 1
-      if winner?(player)
-        puts "#{player.name} WON"
-        self.game_over = true
-      end
-    end
-
-    create_board
+      puts "The game has ended in a stalemate"
+      reset_game
   end
 
   def winner?(player)
-    winning_combinations.any? { |combination| combination.all? { |index| board_array[index] == player.player_symbol } }
+    if winning_combinations.any? do |combination|
+         combination.all? do |index|
+           board_array[index] == player.player_symbol
+         end
+       end
+      puts "#{player.name} HAS WON THE GAME!"
+      self.game_over = true
+      true
+    end
   end
-end
+
+  def stalemate?
+    available_moves <= 0
+  end
+
+  def reset_game
+    puts "Would you like to play again[y/n]"
+    user_input = gets.chomp.downcase
+
+    if user_input == "y"
+      game_over = false
+      available_moves = 9
+      board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      create_board
+    else
+      exit
+    end
+  end
+
+  public
+
+  def place_move(move, player)
+    place_move_helper(move, player)
+  end
 
 class Player
   attr_reader :player_symbol, :name
