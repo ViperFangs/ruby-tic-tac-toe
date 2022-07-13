1bd7df96949bce9d0194468e4b0e37a652afad39
Aarya <amathrej@asu.edu>
2022-07-13 01:40:57 -0700

    Refactor game.rb and main.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,39 +1,18 @@
 class Game
-  attr_accessor :player_one_name, :player_one_symbol, :player_two_name, :player_two_symbol, :board, :user_one_choice,
-                :user_two_choice, :player_one, :player_two, :user_move
+  attr_accessor :board, :player_one, :player_two, :user_move
 
-  def initialize
-    puts 'Welcome to Tic-Tac-Toe'
-    create_users
-    self.board = Board.new
-    play_game until board.game_over
-  end
-
-  def create_users
-    self.player_one_name = user_name('Player 1')
-    self.player_one_symbol = select_symbol(player_one_name)
-
-    self.player_two_name = user_name('Player 2')
-    self.player_two_symbol = select_symbol(player_two_name)
-
-    self.player_one = Player.new(player_one_name, player_one_symbol)
-    self.player_two = Player.new(player_two_name, player_two_symbol)
-    system('clear')
-  end
-
-  def user_name(player)
-    print "\n#{player}, choose your name: "
-    gets.chomp
-  end
-
-  def select_symbol(name)
-    print "\n#{name}, choose your symbol: "
-    gets.chomp
+  def initialize(player_one, player_two, board = Board.new)
+    @player_one = player_one
+    @player_two = player_two
+    @board = board
   end
 
   def play_game
+    return if board.game_over
+
     play_game_helper(player_one)
     play_game_helper(player_two)
+    play_game
   end
 
   def play_game_helper(player)
--- main.rb
+++ main.rb
@@ -2,4 +2,19 @@ require_relative 'lib/board'
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-Game.new
+def create_user(number)
+  puts "Player #{number}, enter your details"
+  print "\tName: "
+  name = gets.chomp
+  print "\tSymbol: "
+  symbol = gets.chomp
+  Player.new(name, symbol)
+end
+
+puts "Welcome to Tic-Tac-Toe\n\n"
+player_one = create_user(1)
+player_two = create_user(2)
+system('clear')
+
+Game.new(player_one, player_two).play_game
+
